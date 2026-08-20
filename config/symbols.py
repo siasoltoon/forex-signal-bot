a878610707ba44aa8c000f0e1e4232560a80e8ca
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-20 20:08:33 +0330

    Add supported symbols and timeframes

---
+++ config/symbols.py
@@ -0,0 +1,38 @@
+"""
+Supported market symbols and timeframes.
+"""
+
+FOREX_SYMBOLS = [
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "USDCHF",
+    "AUDUSD",
+    "USDCAD",
+    "NZDUSD",
+    "EURGBP",
+    "EURJPY",
+    "GBPJPY",
+]
+
+CRYPTO_SYMBOLS = [
+    "BTCUSDT",
+    "ETHUSDT",
+]
+
+STOCK_SYMBOLS = [
+    "AAPL",
+    "MSFT",
+    "NVDA",
+    "TSLA",
+]
+
+TIMEFRAMES = [
+    "1m",
+    "5m",
+    "15m",
+    "30m",
+    "1h",
+    "4h",
+    "1d",
+]
