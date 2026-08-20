cda55e8259e71128e6506c8859a7729dc765c98f
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-21 02:28:10 +0330

    Add graceful shutdown manager

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
+from __future__ import annotations
+
 import asyncio
 
 from app import create_app
+
+from core.shutdown import ShutdownManager
+
+from core.logger import setup_logger
+
+
+logger = setup_logger()
+
 
 
 async def main() -> None:
 
     app = create_app()
 
+    shutdown = ShutdownManager()
+
+    shutdown.setup()
+
+
     await app.start()
+
+
+    logger.info(
+        "Application running."
+    )
+
+
+    await shutdown.wait()
+
+
+    logger.info(
+        "Stopping application..."
+    )
+
+
+    await app.stop()
+
 
 
 if __name__ == "__main__":
 
-    asyncio.run(main())
+    asyncio.run(
+        main()
+    )
