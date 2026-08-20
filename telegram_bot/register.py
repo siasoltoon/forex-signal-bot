fc38ae50387bcdf0031ffde41ae8c51dba95f01a
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-21 00:31:58 +0330

    Improve telegram handler registration

--- telegram_bot/register.py
+++ telegram_bot/register.py
@@ -1,12 +1,23 @@
+from __future__ import annotations
+
 from telegram.ext import Application, CommandHandler
 
 from telegram_bot.handlers.start import start_handler
 
 
-def register_handlers(app: Application):
-    app.add_handler(
+def register_handlers(
+    app: Application,
+) -> None:
+    """
+    Register all Telegram bot handlers.
+    """
+
+    handlers = [
         CommandHandler(
             "start",
-            start_handler
-        )
-    )
+            start_handler,
+        ),
+    ]
+
+    for handler in handlers:
+        app.add_handler(handler)
