165933cb6e41a442c5255dfe18c4f2db43ee2705
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-21 02:49:09 +0330

    Refactor market data providers structure

--- data/providers/__init__.py
+++ data/providers/__init__.py
@@ -1,1 +1,11 @@
 
+from data.providers.oanda import OandaProvider
+from data.providers.finnhub import FinnhubProvider
+from data.providers.alphavantage import AlphaVantageProvider
+
+
+__all__ = [
+    "OandaProvider",
+    "FinnhubProvider",
+    "AlphaVantageProvider",
+]
