5775ec993ed8cedb2205d7be5c8774bf76df8e59
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-23 13:13:12 +0330

    fix: avoid shadowing Python standard platform module

--- platform/__init__.py
+++ platform/__init__.py
@@ -1,11 +1,1 @@
 """Distributed platform contracts and orchestration boundaries."""
-
-from .job_queue import InMemoryJobQueue
-from .runtime_contracts import AlertEvent, Job, JobPriority, JobStatus, WorkerCapabilities, WorkerHeartbeat
-from .scheduler import ResourceAwareScheduler
-from .worker_registry import WorkerRegistry
-
-__all__ = [
-    "AlertEvent", "InMemoryJobQueue", "Job", "JobPriority", "JobStatus",
-    "ResourceAwareScheduler", "WorkerCapabilities", "WorkerHeartbeat", "WorkerRegistry",
-]
