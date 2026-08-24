4422d7419ab2d7547f2bcb7b4118723240d2b011
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-25 01:34:17 +0330

    feat: expose pc worker runtime

--- worker/__init__.py
+++ worker/__init__.py
@@ -2,5 +2,6 @@
 
 from .contracts import JobRequest, JobResult, WorkerCapabilities
 from .dispatcher import WorkerDispatcher
+from .runtime import WorkerRuntime
 
-__all__ = ["JobRequest", "JobResult", "WorkerCapabilities", "WorkerDispatcher"]
+__all__ = ["JobRequest", "JobResult", "WorkerCapabilities", "WorkerDispatcher", "WorkerRuntime"]
