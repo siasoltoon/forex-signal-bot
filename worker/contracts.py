5737e9c3f38d2b54a92f530a39b979495854dff6
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-27 23:59:56 +0330

    feat(worker): add coding agent job contract

--- worker/contracts.py
+++ worker/contracts.py
@@ -5,28 +5,12 @@ from typing import Any
 
 
 HEAVY_JOB_TYPES = {
-    "backtest",
-    "walk_forward",
-    "monte_carlo",
-    "hyperparameter_optimization",
-    "feature_engineering",
-    "multitimeframe_analysis",
-    "heavy_market_scan",
-    "ml_training",
-    "xgboost_training",
-    "lightgbm_training",
-    "random_forest_training",
-    "timeseries_training",
-    "ensemble_training",
-    "candle_batch_analysis",
-    "dataset_build",
-    "model_evaluation",
-    "deep_learning_training",
-    "transformer_training",
-    "lstm_training",
-    "gru_training",
-    "medium_model_training",
-    "multi_agent_analysis",
+    "backtest", "walk_forward", "monte_carlo", "hyperparameter_optimization",
+    "feature_engineering", "multitimeframe_analysis", "heavy_market_scan",
+    "ml_training", "xgboost_training", "lightgbm_training", "random_forest_training",
+    "timeseries_training", "ensemble_training", "candle_batch_analysis", "dataset_build",
+    "model_evaluation", "deep_learning_training", "transformer_training", "lstm_training",
+    "gru_training", "medium_model_training", "multi_agent_analysis", "coding_agent",
 }
 
 
@@ -37,12 +21,8 @@ class WorkerCapabilities:
     max_ram_gb: int = 16
     supported_jobs: frozenset[str] = frozenset(HEAVY_JOB_TYPES)
     limited_jobs: frozenset[str] = frozenset({
-        "deep_learning_training",
-        "transformer_training",
-        "lstm_training",
-        "gru_training",
-        "medium_model_training",
-        "multi_agent_analysis",
+        "deep_learning_training", "transformer_training", "lstm_training", "gru_training",
+        "medium_model_training", "multi_agent_analysis", "coding_agent",
     })
 
 
